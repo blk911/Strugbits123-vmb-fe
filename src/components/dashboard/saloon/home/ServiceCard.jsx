c92973b8ed33c7bf26c5df419d487fd4fb988a68
zainulabideen-strugbits <zainulabideen@strugbitsglobal.com>
2025-12-12 19:27:07 +0500

    fixed  hover and cards

--- src/components/dashboard/saloon/home/ServiceCard.jsx
+++ src/components/dashboard/saloon/home/ServiceCard.jsx
@@ -1,6 +1,6 @@
 export default function ServiceCard({ img, title, desc, price }) {
   return (
-    <div className="border border-[#0000001A] p-4 rounded-[10px] flex flex-col sm:flex-row  gap-4 items-start hover:bg-[#FF92A51A] transition cursor-pointer">
+    <div className="border border-[#0000001A] p-4 rounded-[10px] flex flex-col sm:flex-row  gap-4 items-start bg-[#FF92A51A] hover:bg-white transition cursor-pointer">
       <div className="shrink-0 w-12 h-12 sm:w-14 sm:h-14 border border-[#E5E7EB] rounded-[8px] flex  items-center justify-center overflow-hidden">
         <img src={img} alt={title} className="w-full h-full object-cover" />
       </div>
