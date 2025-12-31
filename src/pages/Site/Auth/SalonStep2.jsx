9e6fef7f3b566daf5b173908c93749215fa3ed36
ZAIN UL ABIDEEN <235869094+zainulabideen-strugbits@users.noreply.github.com>
2025-12-31 16:45:36 +0500

    reverted time input

--- src/pages/Site/Auth/SalonStep2.jsx
+++ src/pages/Site/Auth/SalonStep2.jsx
@@ -198,9 +198,9 @@ export default function SalonStep2({ onBack }) {
       />
 
       <div className="grid grid-cols-3 gap-4 max-sm:grid-cols-1">
-        {/* <TimeInput label="Start Time" name="startTime" />
-        <TimeInput label="End Time" name="endTime" /> */}
-<TimeInput
+        <TimeInput label="Start Time" name="startTime" />
+        <TimeInput label="End Time" name="endTime" />
+{/* <TimeInput
     label="Start Time"
     name="startTime"  
     value={watch("startTime")}
@@ -213,7 +213,7 @@ export default function SalonStep2({ onBack }) {
     value={watch("endTime")}
     onChange={(e) => setValue("endTime", e.target.value, { shouldValidate: true })}
     error={errors.endTime}
-  />
+  /> */}
         <div className="relative" ref={detailsRef}>
           <label className="block text-[#374151] text-[14px] font-semibold mb-1">
             Working Days
